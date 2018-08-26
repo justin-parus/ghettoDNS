ff4dee4558994cd41889eff5d1885b224086f14f
Justin Parus <jparus@umich.edu>
2018-08-25 19:33:57 -0700

    Now uses new "usrMngr" and checks more frequently

--- ghettoDNS.py
+++ ghettoDNS.py
@@ -4,66 +4,67 @@
 import requests
 import smtplib
 import os
+import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+import usrMngr
+
 class GhettoDNS:
-  """Simple DNS Server work-around"""
+  '''Simple DNS Server work-around. Uses free "ip services" and emails user when IP changes'''
+
   def __init__(self):
-    self.lastIP = ''
+    self.email_usr = usrMngr.UsrMngr(True, '')
+    self.last_ip = ''
+    self.email_url = 'gmail.com'
+    self.mail_server = 'smtp.gmail.com:587'
+    # other options 'http://ident.me', 'http://icanhazip.com'
+    self.ip_server = 'https://api.ipify.org'
 
-  # static
-  from_addr = ''
-  to_addrs = ['', '']
-  username = ''
-  password = ''
-  mail_server = 'smtp.gmail.com:587'
-  # other options 'http://ident.me', 'http://icanhazip.com'
-  ip_server = 'https://api.ipify.org'
-
-  def mailIP(self):
+  def mail_ip(self):
+    print("Attempting to check ip and mail at ", datetime.datetime.now())
     try:
-      ip = requests.get(GhettoDNS.ip_server).text
+      ip = requests.get(self.ip_server).text
+      print("  ip is     ", ip)
+      print("  old ip is ", self.last_ip)
 
-      if self.lastIP != ip:
-        self.lastIP = ip
+      if self.last_ip != ip:
+        self.last_ip = ip
 
+        email_addr = self.email_usr.get_username() + '@' + self.email_url
         subject = 'ghettodns public ip'
-        recipients = ''
-        for x in GhettoDNS.to_addrs:
-          recipients += x + ','
-
-        msg = msg = "\r\n".join([
-          "From: " + GhettoDNS.from_addr,
-          "To: " + recipients,
-          "Subject: " + subject,
-          "",
+        msg = msg = '\r\n'.join([
+          'From: ' + email_addr,
+          'To: ' + email_addr,
+          'Subject: ' + subject,
+          '',
           ip
-          ])
+        ])
         
-        server = smtplib.SMTP(GhettoDNS.mail_server)
-        server.ehlo() 
-        server.starttls()
-        server.login(GhettoDNS.username, GhettoDNS.password)
-        server.sendmail(GhettoDNS.from_addr, GhettoDNS.to_addrs, msg)
-        server.close()
+        with smtplib.SMTP(self.mail_server) as server:
+          server.ehlo()
+          server.starttls()
+          server.login(self.email_usr.get_username(), self.email_usr.get_password())
+          server.sendmail(email_addr, email_addr, msg)
         
-        print('Successfully mailed new IP:', ip)
+        print("  successfully mailed new ip")
+      else:
+        print("  no ip change, not mailing")
 
     except requests.exceptions.RequestException as err:
-      print('Failed to get ip:', err)
+      print("Failed to get ip: ", err)
     except smtplib.SMTPException as err:
-      print('Failed to send mail:', err)
+      print("Failed to send mail: ", err)
 
 
 if __name__ == '__main__':
   dns = GhettoDNS()
 
   scheduler = BlockingScheduler()
-  scheduler.add_job(dns.mailIP, 'cron', hour='19', minute='0', second='0')
-  print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+  scheduler.add_job(dns.mail_ip, 'cron', minute='30')
+  print("Press Ctrl+{0} to exit".format('Break' if os.name == 'nt' else 'C'))
 
   try:
-    print('Starting scheduler')
+    print("Starting ghettoDNS scheduler")
     scheduler.start()
   except (KeyboardInterrupt, SystemExit):
-    print('Process ended')
+    print("\nghettoDNS terminated")
